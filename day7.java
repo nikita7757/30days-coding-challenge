1f4b0347ae1e130c270bc354f43b54bd3391f6fc
Nikita Shedage <132663150+nikita7757@users.noreply.github.com>
2024-09-15 12:32:24 +0530

    Create day7.java
     Trapping Rain Water
    You are given an array height[] of non-negative integers where each element represents the height of a bar in a histogram-like structure. These bars are placed next to each other, and the width of each bar is 1 unit. When it rains, water gets trapped between the bars if there are taller bars on both the left and right of the shorter bars. The task is to calculate how much water can be trapped between these bars after the rain.

---
+++ day7.java
@@ -0,0 +1,35 @@
+public class day7 {
+    public static int trap(int[] height) {
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+
+        int n = height.length;
+        int[] leftMax = new int[n];
+        int[] rightMax = new int[n];
+        int waterTrapped = 0;
+
+        leftMax[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+        }
+
+    
+        rightMax[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
+        }
+
+        for (int i = 0; i < n; i++) {
+            waterTrapped += Math.min(leftMax[i], rightMax[i]) - height[i];
+        }
+
+        return waterTrapped;
+    }
+
+    public static void main(String[] args) {
+        int[] height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+        int result = trap(height);
+        System.out.println(result);
+    }
+}
